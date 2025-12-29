8005ae5aa42a85028e47c866eaf1807d2f8ec92d
Ketu Patel <pketu916@gmail.com>
2025-12-29 18:26:34 +0530

    Add SVG path animation and GSAP integration

---
+++ 03 gsap svg anmiation copy/script.js
@@ -0,0 +1,46 @@
+// var intialPath = `M 10 100 Q 500 100 990 100`;
+// var path = `M 10 100 Q 500 100 990 100`;
+
+// var svg = document.querySelector("#svg");
+
+// svg.addEventListener("mousemove", function (dets) {
+//   var path = `M 10 100 Q ${dets.x} ${dets.y} 990 100`;
+
+//   gsap.to("#svg svg path", {
+//     attr: {
+//       d: path,
+//     },
+//     duration: 0.3,
+//     ease: "power3.out",
+//   });
+// });
+
+
+
+// svg.addEventListener("mouseleave", function () {
+
+//   gsap.to("#svg svg path", {
+//     attr: {
+//       d: intialPath,
+//     },
+//     duration: 1.5,
+//     ease: "elastic.out(1,0.2)",
+//   });
+// });
+
+
+var svg = document.querySelector("#svg");
+
+
+  gsap.registerPlugin(MotionPathPlugin);
+
+    gsap.to(".dot", {
+      duration: 5,
+      repeat: -1,
+      ease: "none",
+      motionPath: {
+        path: "#path",
+        align: "#path",
+        alignOrigin: [0.5, 0.5]
+      }
+    });
